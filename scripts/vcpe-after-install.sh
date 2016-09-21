1ab0a56450222ad475eb2e3f8fb5aeed30e3fe68
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2016-09-21 17:05:58 +0300

    Added some improvements

--- scripts/vcpe-after-install.sh
+++ scripts/vcpe-after-install.sh
@@ -5,7 +5,7 @@ add_ssh_pub_key() {
     user=deadman
     su - $user <<THIS_DEATH
         install -d ~/.ssh -m 0700
-        echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW7cG4fczHsXz5iqlgDSY/2D1ENpn8e7WVEU8IN5+i81LqnZ9L3QL+LuDLR+vdWvaRP09me++U6yzALoa0jN6nxPHIkjmMmA5kYdsWCVEMCEd3Q07/xIG0TlYHir4ggv3p7zP6K6PERqRFJ6IvzJRx4hWOqky1IgbQPOnzFs4BheYARDGY3dsepBaCtKQkIT1mlW1VEkKsfb4ARUMaCgv9IPosYsZ+0B8Jh9AQovtBEDoDFC5zXIcFznqbdL4w7iZ/5pQ025viyVnaeoRemTGBOP99+Z7Kh0b+LUt6f03GpikBkblJJM8iEC2sEV3rSrszMk4Ot5dtWhygoS4trtft' >> ~/.ssh/authorized_keys
+        echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW7cG4fczHsXz5iqlgDSY/2D1ENpn8e7WVEU8IN5+i81LqnZ9L3QL+LuDLR+vdWvaRP09me++U6yzALoa0jN6nxPHIkjmMmA5kYdsWCVEMCEd3Q07/xIG0TlYHir4ggv3p7zP6K6PERqRFJ6IvzJRx4hWOqky1IgbQPOnzFs4BheYARDGY3dsepBaCtKQkIT1mlW1VEkKsfb4ARUMaCgv9IPosYsZ+0B8Jh9AQovtBEDoDFC5zXIcFznqbdL4w7iZ/5pQ025viyVnaeoRemTGBOP99+Z7Kh0b+LUt6f03GpikBkblJJM8iEC2sEV3rSrszMk4Ot5dtWhygoS4trtft' > ~/.ssh/authorized_keys
         chmod 0600 ~/.ssh/authorized_keys
 THIS_DEATH
 }
@@ -16,9 +16,25 @@ allow_deadman_sudo() {
     echo 'deadman ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers.d/deadman
 }
 
+# This allows to edit files in place
+set_option() {
+  s_file="$1"
+  key="$2"
+  value="$3"
+  separator="$4"
+
+  grep -qE "^\s*${key}\s*${separator}" $s_file
+  if [ $? -eq 0 ]; then
+      sed -i -e "s/^\s*${key}\s*${separator}.*$/${key}${separator}${value}/g" $s_file
+  else
+      echo "${key}${separator}${value}" >> $s_file
+  fi
+}
+
 update_sshd() {
-    echo 'UseDNS no' >> /etc/ssh/sshd_config
-    sed -i '/PermitRootLogin / s/ .*/ no/' /etc/ssh/sshd_config
+    set_option /etc/ssh/sshd_config "UseDNS"                 "no"    " "
+    set_option /etc/ssh/sshd_config "PermitRootLogin"        "no"    " "
+    set_option /etc/ssh/sshd_config "PasswordAuthentication" "no"    " "
 }
 
 
@@ -26,6 +42,7 @@ update_sshd() {
 add_ssh_pub_key
 allow_deadman_sudo
 update_sshd
+
 
 exit 0
 
