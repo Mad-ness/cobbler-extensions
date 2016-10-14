6c5828eb83e390e65a209bf9b86552e04da72395
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2016-10-14 10:45:06 +0300

    Fixed syntax errors so cobbler recogizes the generated configs as native

--- systems/way2/genprofiles.py
+++ systems/way2/genprofiles.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env 
-
-# -*- codepage: utf-8
-
+#
+# -****- 
+#codingUTF-8 -*-
+#
 import os
 import json
 import yaml
+import time
+import uuid
 import jinja2
 import argparse
 
@@ -23,6 +26,8 @@ def dict2humantext(buffer): return json.dumps(buffer, indent=4)
 def render_systems2plain():
     systems_plaindefs = loadfile2plain(systems_file)
     systems_vars = load_vars(systems_plaindefs)
+    systems_vars['ctime'] = time.time()
+    systems_vars['mtime'] = systems_vars['ctime']
     return render_template(systems_plaindefs, { 'vars': systems_vars })
 
 def load_vars(buffer):
@@ -31,7 +36,7 @@ def load_vars(buffer):
     if data is not None and 'vars' in data.keys(): result = data['vars']
     return result
 
-def render_systems2yaml():
+def render_systems2yaml(output_format):
     # get rendered systems file just a text
     systemsfile_plaintext = render_systems2plain()
     # convert it to a dict structure, vars are here as well 
@@ -46,8 +51,18 @@ def render_systems2yaml():
         sysnames = []
         for sys in systems_defs_yaml['systems']:
             sysnames.append(sys['name'])
-            with open(sys['name'] + '.output.yaml', 'w') as fd:
-                fd.write(render_template(system_template, sys))
+            sys['uid'] = uuid.uuid4()
+            if output_format == 'json':
+                data_json = json.dumps(yaml2dict(render_template(system_template, sys)))
+                with open(sys['name'] + '.output.yaml', 'w') as fd:
+                    fd.write(data_json)
+            elif output_format == 'yaml':
+                print("\n[ BEGIN ] System name: %s\n" % sys['name'])
+                print(render_template(system_template, sys))
+                print("\n[ END ] System name: %s\n" % sys['name'])
+            else:
+                print("Unknown format given: %s. Do nothing." % output_format)
+                exit(1)
         print("Processed systems:\n - " + "\n - ".join(sysnames))
             
 def menu():
@@ -60,14 +75,15 @@ def menu():
 
     parser.add_argument("-s", "--systems-file", default=f2, help="Name of a file within systems defitions at YAML syntax (default: %s)" % f2)
     parser.add_argument("-t", "--template-file", default=f1, help="Template file that to be used for making output config files (default: %s)" % f1)
-    #parser.add_argument("-o", "--overwrite", action='store_false', dest="boolean_switch", default=False, help="Overwrite an output file in place if it exists")
+    parser.add_argument("-o", "--output-format", choices=["json", "yaml"], default="yaml", type=str, help="Output format: json, yaml. If json is typed when output is written in files otherwise it is printed out at a screen")
 
     args = parser.parse_args()
 
     template_file = args.template_file
     systems_file = args.systems_file
 
-    render_systems2yaml()
+    render_systems2yaml(output_format=args.output_format)
+
 
 if __name__ == "__main__":
     menu()
